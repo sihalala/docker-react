e3d3d361d98db8b727e300bc9699e6738217daaa
Ho Ha <htsiha@gmail.com>
2019-11-06 18:07:37 +0700

    new branch committed

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hi Haho
+          Edit <code>src/App.js</code> ANH YEU EM
         </p>
         <a
           className="App-link"
